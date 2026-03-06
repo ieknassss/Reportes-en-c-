081dd3c5eb51752fdad9b67ee0c70fa732004a0d
Christopher <chrissan1801@gmail.com>
2026-03-06 08:43:51 -0400

    Se agregan clases de dominio para cliente, prestamo, pago y mora

---
+++ REPORTES/Domain/Prestamo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace REPORTES.Domain
+{
+    public class Prestamo
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public decimal Monto { get; set; }
+        public int Meses { get; set; }
+        public decimal InteresGenerado { get; set; }
+        public decimal MontoTotal { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+
+        public decimal CalcularSaldoPendiente(decimal totalPagado)
+        {
+            decimal saldo = MontoTotal - totalPagado;
+
+            if (saldo < 0)
+                saldo = 0;
+
+            return saldo;
+        }
+    }
+}
