685cdcd94df41f24b5f371124c27ae42009f3aee
Iekna Lora <ieknalora@gmail.com>
2026-03-07 18:38:09 -0400

    Formulario gestion de pagos creado

--- REPORTES/Interfaces/FrmGestionPagos.cs
+++ REPORTES/Interfaces/FrmGestionPagos.cs
@@ -16,5 +16,55 @@ namespace REPORTES.Interfaces
         {
             InitializeComponent();
         }
+
+        private void btnCalcularPago_Click(object sender, EventArgs e)
+        {
+            double montoAnterior = Convert.ToDouble(txtMontoAnterior.Text);
+            double cuota = Convert.ToDouble(txtCuota.Text);
+
+            double interes = montoAnterior * 0.02; 
+
+            double nuevoMonto = montoAnterior - cuota;
+
+            txtInteres.Text = interes.ToString("N2");
+            txtNuevoMonto.Text = nuevoMonto.ToString("N2");
+        }
+
+        private void btnRegistrarPago_Click(object sender, EventArgs e)
+        {
+            if (cmbCliente.Text == "" || txtCuota.Text == "")
+            {
+                MessageBox.Show("Debe completar los campos");
+                return;
+            }
+
+            MessageBox.Show("Pago registrado correctamente");
+
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            cmbCliente.SelectedIndex = -1;
+            txtMontoAnterior.Clear();
+            txtInteres.Clear();
+            txtCuota.Clear();
+            txtNuevoMonto.Clear();
+            txtMesesRestantes.Clear();
+            txtTotalIntereses.Clear();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+          
+            cmbCliente.SelectedIndex = -1;
+            txtMontoAnterior.Clear();
+            txtInteres.Clear();
+            txtCuota.Clear();
+            txtNuevoMonto.Clear();
+            txtMesesRestantes.Clear();
+            txtTotalIntereses.Clear();
+        
+    }
     }
 }
