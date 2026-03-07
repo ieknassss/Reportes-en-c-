1f1e65ca02fdb8edfcaed61bf8931dfc958991d0
Iekna Lora <ieknalora@gmail.com>
2026-03-07 17:20:58 -0400

    Formulario solicitud de prestamo con calculos listo

--- REPORTES/Interfaces/FrmSolicitudPrestamo.Designer.cs
+++ REPORTES/Interfaces/FrmSolicitudPrestamo.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSolicitudPrestamo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCliente = new System.Windows.Forms.ComboBox();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.txtMeses = new System.Windows.Forms.TextBox();
+            this.txtInteres = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtCuota = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SOLICITUD DE PRÉSTAMO";
+            // 
+            // cmbCliente
+            // 
+            this.cmbCliente.FormattingEnabled = true;
+            this.cmbCliente.Location = new System.Drawing.Point(245, 121);
+            this.cmbCliente.Name = "cmbCliente";
+            this.cmbCliente.Size = new System.Drawing.Size(381, 28);
+            this.cmbCliente.TabIndex = 1;
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(245, 178);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(381, 26);
+            this.txtMonto.TabIndex = 2;
+            // 
+            // txtMeses
+            // 
+            this.txtMeses.Location = new System.Drawing.Point(245, 233);
+            this.txtMeses.Name = "txtMeses";
+            this.txtMeses.Size = new System.Drawing.Size(381, 26);
+            this.txtMeses.TabIndex = 3;
+            // 
+            // txtInteres
+            // 
+            this.txtInteres.Location = new System.Drawing.Point(245, 294);
+            this.txtInteres.Name = "txtInteres";
+            this.txtInteres.ReadOnly = true;
+            this.txtInteres.Size = new System.Drawing.Size(381, 26);
+            this.txtInteres.TabIndex = 4;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(245, 355);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(381, 26);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // txtCuota
+            // 
+            this.txtCuota.Location = new System.Drawing.Point(245, 410);
+            this.txtCuota.Name = "txtCuota";
+            this.txtCuota.ReadOnly = true;
+            this.txtCuota.Size = new System.Drawing.Size(381, 26);
+            this.txtCuota.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cliente: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 30);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Monto: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Meses: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(56, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Interés: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(56, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 30);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Monto Total: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(56, 406);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 30);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Cuota Mensual:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnGuardar.Location = new System.Drawing.Point(372, 492);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(308, 32);
+            this.btnGuardar.TabIndex = 13;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.LightBlue;
+            this.btnCalcular.Location = new System.Drawing.Point(27, 492);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(323, 32);
+            this.btnCalcular.TabIndex = 14;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.White;
+            this.btnLimpiar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLimpiar.BackgroundImage")));
+            this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLimpiar.Location = new System.Drawing.Point(633, 40);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(68, 50);
+            this.btnLimpiar.TabIndex = 15;
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // FrmSolicitudPrestamo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmReporteProductos";
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(728, 562);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCuota);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtInteres);
+            this.Controls.Add(this.txtMeses);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.cmbCliente);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmSolicitudPrestamo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Solicitud de Préstamo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCliente;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.TextBox txtMeses;
+        private System.Windows.Forms.TextBox txtInteres;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtCuota;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
     }
 }
--- REPORTES/Interfaces/FrmSolicitudPrestamo.cs
+++ REPORTES/Interfaces/FrmSolicitudPrestamo.cs
@@ -16,5 +16,63 @@ namespace REPORTES.Interfaces
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double monto = Convert.ToDouble(txtMonto.Text);
+            int meses = Convert.ToInt32(txtMeses.Text);
+
+            double tasa = 0;
+
+            if (meses <= 12)
+                tasa = 13.25;
+            else if (meses <= 24)
+                tasa = 15;
+            else
+                tasa = 30;
+
+            double interes = monto * (tasa / 100);
+            double total = monto + interes;
+            double cuota = total / meses;
+
+            txtInteres.Text = interes.ToString("N2");
+            txtTotal.Text = total.ToString("N2");
+            txtCuota.Text = cuota.ToString("N2");
+
+           
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtMonto.Clear();
+            txtMeses.Clear();
+            txtInteres.Clear();
+            txtTotal.Clear();
+            txtCuota.Clear();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (cmbCliente.Text == "" || txtMonto.Text == "" || txtMeses.Text == "")
+            {
+                MessageBox.Show("Debe completar todos los campos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            MessageBox.Show("Préstamo registrado correctamente", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            LimpiarCampos();
+     
+        }
+
+        private void LimpiarCampos()
+        {
+            cmbCliente.SelectedIndex = -1;
+            txtMonto.Clear();
+            txtMeses.Clear();
+            txtInteres.Clear();
+            txtTotal.Clear();
+            txtCuota.Clear();
+        }
     }
 }
